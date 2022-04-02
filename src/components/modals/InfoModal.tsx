aa8e88b0ca0fc0c7db4e80ece2a792b616a0765b
Charles Ming <charlesming@berkeley.edu>
2022-04-01 23:19:01 -0700

    start customizing

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -58,13 +58,12 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        Made with {'<3'} from CM to LX. Based on -{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          check out og code
         </a>{' '}
       </p>
     </BaseModal>
