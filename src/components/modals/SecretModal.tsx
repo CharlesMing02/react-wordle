3f68cedd4411e333e308b37b632951890ba6506c
Charles Ming <charlesming@berkeley.edu>
2022-04-12 14:28:13 -0700

    fix transitions

--- src/components/modals/SecretModal.tsx
+++ src/components/modals/SecretModal.tsx
@@ -49,47 +49,61 @@ export const SecretModal = ({
 
   return (
     <BaseModal title="" isOpen={isOpen} handleClose={handleClose}>
-      <div className={'flex flex-col mt-2 divide-y ' + jiggleState}>
-        {!showMessage ? (
-          <>
-            <div className="w-full max-w-sm">
-              <p>┌(▀Ĺ̯ ▀-͠ )┐</p>
-              <p>What's the secret</p>
-              <div className="flex items-center border-b border-purple-300 py-2">
-                <input
-                  value={secretInput}
-                  onChange={handleChange}
-                  className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                  type="text"
-                  placeholder="secret"
-                  aria-label="Full name"
-                />
-                <button
-                  className="flex-shrink-0 bg-purple-300 hover:bg-purple-500 border-purple-300 hover:border-purple-500 text-sm border-4 text-white py-1 px-2 rounded"
-                  type="button"
-                  onClick={handleSubmit}
-                >
-                  Enter
-                </button>
-              </div>
-            </div>
-          </>
-        ) : (
+      <div className={'h-96 flex flex-col mt-2 justify-center ' + jiggleState}>
+        {/* {!showMessage ? ( */}
+        <>
           <Transition
-            show={showMessage}
+            appear={true}
+            show={!showMessage}
             as={Fragment}
-            enter="ease-out duration-2000 transition"
+            enter="transition-opacity duration-500"
             enterFrom="opacity-0"
             enterTo="opacity-100"
-            leave="transition ease-in duration-100"
+            leave="transition-opacity duration-0"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="w-full max-w-sm">
+            <div className="flex flex-row justify-center">
+              <div className="w-60 max-w-sm">
+                <p>┌(▀Ĺ̯ ▀-͠ )┐</p>
+                <p>What's the secret</p>
+                <div className="flex items-center border-b border-purple-300 py-2">
+                  <input
+                    value={secretInput}
+                    onChange={handleChange}
+                    className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    type="text"
+                    placeholder="secret"
+                    aria-label="Full name"
+                  />
+                  <button
+                    className="flex-shrink-0 bg-purple-300 hover:bg-purple-500 border-purple-300 hover:border-purple-500 text-sm border-4 text-white py-1 px-2 rounded"
+                    type="button"
+                    onClick={handleSubmit}
+                  >
+                    Enter
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Transition>
+          {/* ) : ( */}
+          <Transition
+            appear={true}
+            show={showMessage}
+            as={Fragment}
+            enter="transition-opacity duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="w-full max-w-sm flex flex-row justify-center">
               <p className="w-60">{MESSAGE}</p>
             </div>
           </Transition>
-        )}
+        </>
       </div>
     </BaseModal>
   )
